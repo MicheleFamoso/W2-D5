65e871d942ecf25d9284fa921782e88d96dddf25
MicheleFamoso <michelefamoso@gmail.com>
2025-02-14 16:00:16 +0100

    Add title consol.log

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -12,26 +12,26 @@ REGOLE
 */
 const pets = ["dog", "cat", "hamster", "redfish"]
 for (let i = 0; i < pets.length; i++) {
-  console.log(pets[i])
+  console.log("pet", pets[i])
 }
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
-console.log(pets.sort())
+console.log("Ordine alfabetico", pets.sort())
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
-console.log(pets.reverse())
+console.log("Ordine Invertito", pets.reverse())
 
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
 pets.shift()
 pets.push("redfish")
-console.log(pets)
+console.log("Cambio posizione", pets)
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
@@ -58,7 +58,7 @@ const cars = [
 
 for (let i = 0; i < cars.length; i++) {
   cars[i].licensePlate = Math.floor(Math.random() * 20000)
-  console.log(cars[i])
+  console.log("LincensePlate", cars[i])
 }
 
 /* ESERCIZIO 6
@@ -72,7 +72,12 @@ let alfa = {
   trims: ["Q4", "Sport", "Tributo"],
 }
 cars.push(alfa)
-console.log(cars)
+console.log("Aggiunta Alfa Romeo", cars)
+
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop()
+}
+console.log("Rimosso ultimo elemento di Trims", cars)
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
@@ -81,7 +86,7 @@ const justTrims = []
 for (let i = 0; i < cars.length; i++) {
   justTrims.push(cars[i].trims[0])
 }
-console.log(justTrims)
+console.log("nuovo array justTrims", justTrims)
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
@@ -102,8 +107,8 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
 let i = 0
-while (numericArray[i] !== 32) {
-  console.log(numericArray[i])
+while (numericArray[i] !== 66) {
+  console.log("Esercizio 9", numericArray[i])
   i++
 }
 
@@ -183,4 +188,4 @@ for (let i = 0; i < charactersArray.length; i++) {
       console.log("carattere non trovato")
   }
 }
-console.log(position)
+console.log("Esercizio 10", position)
